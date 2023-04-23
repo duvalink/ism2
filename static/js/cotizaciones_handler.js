4e8a8fe1012213ab305407b9a38bd78dc80d316b
duvalink <jose86mend@gmail.com>
2023-04-22 22:23:10 -0700

    Ajustes menores. Se elimino codigo comentado de archivos de js

--- static/js/cotizaciones_handler.js
+++ static/js/cotizaciones_handler.js
@@ -1,67 +1,3 @@
-// $(document).ready(function () {
-//     $('#cliente-select').change(function (event) {
-//         // Establece el valor del campo oculto con el valor seleccionado del cliente
-//         $('#hidden-cliente-id').val(event.target.value);
-//     });
-
-//     $('#save-to-db').click(function () {
-//         console.log('Botón guardar en base de datos presionado');
-//         var cliente_id = $('#hidden-cliente-id').val();
-
-//         $('#hidden-cliente-id').val($('#cliente-select').val());
-//         // Envía el formulario oculto
-//         $('#hidden-form').submit();
-
-//         $.ajax({
-//             url: guardarCotizacionUrl,
-//             method: "POST",
-//             data: {
-//                 cliente_id: cliente_id,
-//             },
-//             success: function (response) {
-//                 console.log('Solicitud AJAX completada con éxito');
-//                 if (response.status === "success") {
-//                     limpiarCotizaciones();
-//                     window.location.href = `/descargar_cotizacion/${response.id_presupuesto}`;
-//                 } else {
-//                     // Muestra un mensaje de error si el estado no es "success"
-//                     console.log('Error en el proceso de guardar la cotización');
-//                 }
-//             },
-//             error: function (jqXHR, textStatus, errorThrown) {
-//                 console.log('Error en la solicitud AJAX:', textStatus, errorThrown);
-//                 // Haz algo en caso de que ocurra un error, por ejemplo, mostrar un mensaje de error
-//             }
-//         });
-//     });
-// });
-
-// function cargarCotizaciones() {
-//     const cotizaciones = JSON.parse(localStorage.getItem('cotizaciones'));
-//     if (cotizaciones) {
-//         for (let cotizacion of cotizaciones) {
-//             agregarFila(cotizacion.descripcion, cotizacion.cantidad, cotizacion.precio, cotizacion.material, cotizacion.importe);
-//         }
-//     }
-// }
-
-// // Guarda las cotizaciones en el almacenamiento local
-// function guardarCotizaciones(cotizaciones) {
-//     localStorage.setItem('cotizaciones', JSON.stringify(cotizaciones));
-// }
-
-// // Limpia las cotizaciones del almacenamiento local
-// function limpiarCotizaciones() {
-//     localStorage.removeItem('cotizaciones');
-// }
-
-
-
-
-
-
-
-
 $(document).ready(function () {
     $('#cliente-select').change(function (event) {
         // Establece el valor del campo oculto con el valor seleccionado del cliente
@@ -100,8 +36,8 @@ $(document).ready(function () {
 });
 
 function cargarCotizaciones() {
-    const cotizaciones = JSON.parse(localStorage.getItem('cotizaciones'));
-    if (cotizaciones) {
+    const cotizaciones = JSON.parse(localStorage.getItem('cotizaciones')); // Obtiene las cotizaciones del almacenamiento local
+    if (cotizaciones) { // Si hay cotizaciones en el almacenamiento local, las agrega a la tabla
         for (let cotizacion of cotizaciones) {
             agregarFila(cotizacion.descripcion, cotizacion.cantidad, cotizacion.precio, cotizacion.material, cotizacion.importe);
         }
